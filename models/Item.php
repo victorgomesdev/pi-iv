11114f2240f9efec3c32cf93ff0955527fda22d0
Victor Gomes Nogueira <victorgomesnog123@gmail.com>
2023-10-03 14:52:14 -0300

    Implementando cadastro de produto no estoque e registro de tansação

--- models/Item.php
+++ models/Item.php
@@ -22,30 +22,93 @@ class Item
             $conn = Conexao::conectar();
 
             $req = $conn->prepare($query);
-            $req->execute(array('id'=>$this->id_produto, 'descricao'=>$this->descricao, 'id_categoria'=> $this->id_categoria));
+            $req->execute(array('id' => $this->id_produto, 'descricao' => $this->descricao, 'id_categoria' => $this->id_categoria));
 
             $conn = null;
             return true;
-
         } catch (PDOException $err) {
-            return $err->errorInfo;
+
+            $conn = null;
+            return false;
         }
     }
 
-    public function excluir_item(): bool
+    public static function excluir_item(int $id): bool
     {
         try {
             $query = 'DELETE FROM itens WHERE id = :id;';
             $conn = Conexao::conectar();
 
             $req = $conn->prepare($query);
-            $req->execute(['id'=>$this->id_produto]);
+            $req->execute(['id' => $id]);
 
             $conn = null;
             return true;
-
         } catch (PDOException $err) {
-            return $err->errorInfo;
+            $conn = null;
+            return false;
+        }
+    }
+
+    public static function editar_item(int $id_produto, string $descricao, int $id_categoria): bool
+    {
+        try {
+            $query = 'UPDATE itens SET id_produto = :id_produto, descricao = :descricao, id_categoria = :id_categoria WHERE id_produto = :$id_produto;';
+            $conn = Conexao::conectar();
+
+            $req = $conn->prepare($query);
+            $req->execute(['id_produto' => $id_produto, 'descricao' => $descricao, 'id_categoria' => $id_categoria]);
+
+            $conn = null;
+            return true;
+        } catch (PDOException $err) {
+            $conn = null;
+            return false;
+        }
+    }
+
+    public static function listar_item(int $id = null)
+    {
+
+        if ($id == null) {
+
+            try {
+
+                $query = 'SELECT * FROM itens;';
+                $conn = Conexao::conectar();
+
+                $req = $conn->prepare($query);
+                $req->execute();
+
+                $res = $req->fetchAll();
+                $conn = null;
+
+                return $res;
+            } catch (PDOException $err) {
+
+                $conn = null;
+
+                return false;
+            }
+        } else {
+
+            try {
+
+                $query = 'SELECT * FROM itens WHERE id_produto = :id;';
+                $conn = Conexao::conectar();
+
+                $req = $conn->prepare($query);
+                $req->execute(['id' => $id]);
+
+                $res = $req->fetchAll();
+                $conn = null;
+
+                return $res;
+            } catch (PDOException $err) {
+
+                $conn = null;
+                return false;
+            }
         }
     }
 }
--- models/Transacao.php
+++ models/Transacao.php
@@ -2,11 +2,77 @@
 
 namespace Models;
 
-class Transacao{
+use PDOException;
 
-    public function __construct()
+class Transacao
+{
+
+    public function __construct(private int $id, private int $tipo, private string $data, private int $usuario, private int $clinica)
     {
-        
+        $this->id = $id;
+        $this->tipo = $tipo;
+        $this->data = $data;
+        $this->usuario = $usuario;
+        $this->clinica = $clinica;
+    }
+
+    public function registar(): bool
+    {
+        try {
+
+            $query = 'INSERT INTO transacoes(id_transacao, id_tipo_tran, data_tran, id_usuario, id_clinica) VALUES (:id, :tipo, :data_tran, :usuario, :clinica);';
+            $conn = Conexao::conectar();
+
+            $req = $conn->prepare($query);
+
+            $req->execute(['id' => $this->id, 'tipo' => $this->tipo, 'data_tran' => $this->data, 'usuario' => $this->usuario, 'clinica' => $this->clinica]);
+
+            $conn = null;
+            return true;
+        } catch (PDOException $err) {
+            return false;
+        }
+    }
+
+    public static function listar(int $tipo = null)
+    {
+
+        if ($tipo == null) {
+
+            try {
+
+                $query = 'SELECT * FROM transacoes;';
+                $conn = Conexao::conectar();
+
+                $req = $conn->prepare($query);
+
+                $req->execute();
+
+                $res = $req->fetchAll();
+
+                $conn = null;
+                return $res;
+            } catch (PDOException $err) {
+            }
+        } else {
+
+            try {
+
+                $query = 'SELECT * FROM transacoes WHERE id_tipo_tran = :tipo;';
+                $conn = Conexao::conectar();
+
+                $req = $conn->prepare($query);
+
+                $req->execute(['tipo'=> $tipo]);
+
+                $res = $req->fetchAll();
+
+                $conn = null;
+                return $res;
+            } catch (PDOException $err) {
+                $conn = null;
+                return false;
+            }
+        }
     }
 }
-?>
